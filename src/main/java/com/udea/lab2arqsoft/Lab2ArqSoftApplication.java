0e17aac13b3553028096defefa93a45fe074bbf2
JoseGomez14 <jose.gomez14@udea.edu.co>
2024-04-15 11:32:59 -0500

    :tada: Begin project
    Co-authored-by: daniel-lujan <daniel.lujan@udea.edu.co>
    Co-authored-by: GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
    Co-authored-by: anavegaa <ana.vega@udea.edu.co>
    Co-authored-by: MaritzaTC <maritza.tabarezc@udea.edu.co>
    Co-authored-by: JuanPablo-A <jpablo.arango1@udea.edu.co>

---
+++ src/main/java/com/udea/lab2arqsoft/Lab2ArqSoftApplication.java
@@ -0,0 +1,13 @@
+package com.udea.lab2arqsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2ArqSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2ArqSoftApplication.class, args);
+	}
+
+}
