ccd34874af10cf97099ede02314323a2ccb8fc17
JoseGomez14 <jose.gomez14@udea.edu.co>
2024-04-15 12:20:10 -0500

    test: :white_check_mark: Add unit testing for data REST Controller
    Co-authored-by: daniel-lujan <daniel.lujan@udea.edu.co>
    Co-authored-by: GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
    Co-authored-by: anavegaa <ana.vega@udea.edu.co>
    Co-authored-by: MaritzaTC <maritza.tabarezc@udea.edu.co>
    Co-authored-by: JuanPablo-A <jpablo.arango1@udea.edu.co>

--- src/test/java/com/udea/lab2arqsoft/Lab2ArqSoftApplicationTests.java
+++ src/test/java/com/udea/lab2arqsoft/Lab2ArqSoftApplicationTests.java
@@ -1,13 +1,43 @@
 package com.udea.lab2arqsoft;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Lab2ArqSoftApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    DataController dataController;
+
+    @Test
+    void health() {
+        assertEquals("APPLICATION WORKING: OK!", dataController.healthCheck());
+    }
+
+    @Test
+    void version() {
+        assertEquals("ACTUAL VERSION: 1.0.0", dataController.version());
+    }
+
+    @Test
+    void nationLength() {
+        Integer nationsLength = dataController.getRandomnNations().size();
+        assertEquals(10, nationsLength);
+    }
+
+    @Test
+    void currenciesLength() {
+        Integer currenciesLength = dataController.getRandomnCurrencies().size();
+        assertEquals(20, currenciesLength);
+    }
+
+    @Test
+    void airlinesLength() {
+        Integer airlinesLength = dataController.getRandomnAirlines().size();
+        assertEquals(20, airlinesLength);
+    }
 
 }
